96eeb7a5bd6fec608e4f57a1d207870b0ff47ed4
hmontes <hmontes@cofar.com.bo>
2020-09-25 17:14:58 -0400

    Ocultando datos

--- src/app/curso/curso.component.ts
+++ src/app/curso/curso.component.ts
@@ -12,14 +12,23 @@ export class CursoComponent implements OnInit {
   public title = 'Componente Curso';
   // Declarar una propiedad de tipo Profesor
   public profesor: Profesor;
+  // Declarar una propiedad de tipo boolean
+  public mostrar_datos:boolean;
+
   // Definir un constructor
   constructor() {
     // Instanciar un objeto Profesor
     this.profesor=new Profesor('Juan Perez', 5600.50, true);
+    // Inicializar en false
+    this.mostrar_datos=true;
   }
   // Definir el metodo inicial
   ngOnInit(): void {
     // Mostrar por consola el objeto
     console.log(this.profesor);
+  }
+  // Definir una funcion que cambia el estado de mostrar_datos
+  cambiarEstado(valor){
+    this.mostrar_datos=valor;
   }
 }
