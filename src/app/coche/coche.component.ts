c50421e295156b0172b79de291fd6d724b143947
hmontes <hmontes@cofar.com.bo>
2020-10-05 14:00:52 -0400

    Complentando formulario de coches

--- src/app/coche/coche.component.ts
+++ src/app/coche/coche.component.ts
@@ -8,14 +8,24 @@ import { Coche } from '../model/coche';
   styleUrls: ['./coche.component.css']
 })
 export class CocheComponent implements OnInit {
+
   // Definir propiedad de titulo
   public title: string;
   // Definir una propiedad coche
   public coche: Coche;
+  // Definir un array de objetos coche
+  public coches:Array<Coche>;
+
   // Inicializar las propiedades
   constructor() { 
     this.title = 'Componente Coche';
     this.coche = new Coche('',0,'');
+    // Inicializar el array de coches
+    this.coches=[
+      new Coche('Totoya',25000,'#000'),
+      new Coche('Subaru',15000,'#00f'),
+      new Coche('BMW',28000,'#f00')
+    ];
   }
 
   ngOnInit(): void {
@@ -25,5 +35,9 @@ export class CocheComponent implements OnInit {
   onSubmit(){
     // Para probar mostrar los datos enviados
     console.log(this.coche);
+    // Adicionar el objeto a la coleccion
+    this.coches.push(this.coche);
+    // Limpiar el formulario
+    this.coche = new Coche('',0,'');
   }
 }
